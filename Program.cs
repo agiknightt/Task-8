e6a427b7b9ad62ed90f206e046e862b7b23fcd31
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-08-31 21:54:15 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exit = "exit";
+            string userInput;
+            bool enterOrExit = true;
+
+            while (enterOrExit)
+            {
+                Console.Write("Для выхода из программы введите exit : ");
+                userInput = Console.ReadLine();
+
+                if(exit == userInput)
+                {
+                    enterOrExit = false;                    
+                }
+                Console.WriteLine("Вы ввели другое слово или фразу.");
+            }
+        }
+    }
+}
